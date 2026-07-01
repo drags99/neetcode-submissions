9bc0ece7c75a2b07d57a7c4882afe3c06483a836
Subin Varghese <srvarghese95@gmail.com>
2026-07-01 12:24:34 -0500

    Add: house-robber-ii - submission-6

---
+++ Data Structures & Algorithms/house-robber-ii/submission-6.py
@@ -0,0 +1,47 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+
+        if len(nums) == 1:
+            return nums[0]
+
+        if len(nums) == 2:
+            return max(nums)
+
+        dp = [0,0]
+        dual_dp = {}
+
+        # pass starting index, and takes
+        # 1 and 2 steps backwards
+        def search(start,end)->int:
+            key = f"{start}_{end}"
+            if key in dual_dp.keys():
+                return dual_dp[key]
+            if end < 0:
+                return 0
+            if start == end:
+                return 0
+            sub_nums = nums[start:end]
+            if len(sub_nums) <= 0:
+                return 0
+            # take 2 steps back or 3 and search
+            step_2 = search(start,end-2) + sub_nums[-1]
+            step_3 = search(start,end-3) + sub_nums[-1]
+            s_max = max(step_2,step_3)
+            dual_dp[key] = s_max
+            return s_max
+
+        for i in range(len(nums)):
+
+            without_self = search(0,i)
+            with_self = search(1,i+1)
+            temp = max(dp[-1],with_self,without_self)
+
+            dp.append(with_self)
+            dp.append(without_self)
+
+
+        return max(dp)
+
+        
